0b779ecdad1176e3479060f2257a399dc1b910d3
Brad Wehrwein <131805643+wehrwein1-bjss@users.noreply.github.com>
2023-05-02 13:18:14 -0400

    #2180 adjust tests to boto3 client default region param removal/revert

--- examples/multiple_os_custom/src/critical_patching/tests/test_apply_association_function.py
+++ examples/multiple_os_custom/src/critical_patching/tests/test_apply_association_function.py
@@ -4,7 +4,6 @@ from moto import mock_ssm
 from typing import List, Dict
 # (Apr 2023) moto does not support list_associations()/start_associations_once(), must be manually mocked in each test case
 #   test approach using monkeypatch adapted from https://stackoverflow.com/a/70127210
-from critical_patching import apply_association_function # importing here OK due to use of monkeypatching, this reference requires __init__.py files
 
 class TestState:
     ssmclient_list_associations_call = None
@@ -15,7 +14,7 @@ def state():
     return TestState()
 
 @pytest.fixture
-def mock_aws_environment(autouse=True):
+def mock_aws_environment(autouse=True): # TODO FIXME for reasons I don't understand, autouse=True doesn't seem to work? Therefore injected manually in each test
     # https://docs.getmoto.org/en/latest/docs/getting_started.html#how-do-i-avoid-tests-from-mutating-my-real-infrastructure
     # https://boto3.amazonaws.com/v1/documentation/api/latest/guide/configuration.html#using-environment-variables
     os.environ["AWS_ACCESS_KEY_ID"]     = "testing"
@@ -25,7 +24,7 @@ def mock_aws_environment(autouse=True):
     os.environ["AWS_DEFAULT_REGION"]    = "eu-west-2"
 
 @mock_ssm
-def test_apply_association_lambda_happy_path(state: TestState, monkeypatch):
+def test_apply_association_lambda_happy_path(state: TestState, monkeypatch, mock_aws_environment):
     # setup
     def ssmclient_list_associations_mock(AssociationFilterList: List) -> Dict:
         state.ssmclient_list_associations_call = AssociationFilterList
@@ -33,6 +32,7 @@ def test_apply_association_lambda_happy_path(state: TestState, monkeypatch):
     def ssmclient_start_associations_once_mock(AssociationIds: List) -> None:
         state.ssmclient_start_associations_call = AssociationIds
         return None # success
+    from critical_patching import apply_association_function # import after AWS env setup (accept default region)
     monkeypatch.setattr(apply_association_function.ssm_client, "list_associations", ssmclient_list_associations_mock)
     monkeypatch.setattr(apply_association_function.ssm_client, "start_associations_once", ssmclient_start_associations_once_mock)
     # invoke lambda, assert results
@@ -41,11 +41,12 @@ def test_apply_association_lambda_happy_path(state: TestState, monkeypatch):
     assert state.ssmclient_start_associations_call == ["assn_1"]
 
 @mock_ssm
-def test_apply_association_lambda_no_listed_associations_throws_exception(state: TestState, monkeypatch):
+def test_apply_association_lambda_no_listed_associations_throws_exception(state: TestState, monkeypatch, mock_aws_environment):
     # setup
     def ssmclient_list_associations_mock(AssociationFilterList: List) -> Dict:
         state.ssmclient_list_associations_call = AssociationFilterList
         return { "Associations" : [] } # empty list associations
+    from critical_patching import apply_association_function # import after AWS env setup (accept default region)
     monkeypatch.setattr(apply_association_function.ssm_client, "list_associations", ssmclient_list_associations_mock)
     # invoke lambda, assert results
     with pytest.raises(IndexError, match="list index out of range") as e_context:
--- examples/multiple_os_custom/src/critical_patching/tests/test_update_baseline_function.py
+++ examples/multiple_os_custom/src/critical_patching/tests/test_update_baseline_function.py
@@ -4,7 +4,7 @@ from moto import mock_ec2
 from critical_patching.tests.ec2s_test_infra import new_boto3_ec2_resource, new_mocked_ec2_with_tags
 
 @pytest.fixture
-def mock_aws_environment(autouse=True):
+def mock_aws_environment(autouse=True): # TODO FIXME for reasons I don't understand, autouse=True doesn't seem to work? Therefore injected manually in each test
     # https://docs.getmoto.org/en/latest/docs/getting_started.html#how-do-i-avoid-tests-from-mutating-my-real-infrastructure
     os.environ["AWS_ACCESS_KEY_ID"]     = "testing"
     os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
@@ -13,14 +13,14 @@ def mock_aws_environment(autouse=True):
     os.environ["AWS_DEFAULT_REGION"]    = "eu-west-2"
 
 @mock_ec2
-def test_update_baseline_lambda_no_instances_to_patch():
+def test_update_baseline_lambda_no_instances_to_patch(mock_aws_environment):
     # setup: none
     # invoke lambda
     from critical_patching import update_baseline_function # accept the moto mocked client, need __init__.py files for module reference
     assert update_baseline_function.lambda_handler(event={"patch_group": "PatchGroup_1"}, context=None) == [] # no EC2s == nothing to do.
 
 @mock_ec2
-def test_update_baseline_lambda_happy_path():
+def test_update_baseline_lambda_happy_path(mock_aws_environment):
     # setup
     from critical_patching import update_baseline_function # accept the moto mocked client, need __init__.py files for module reference
     instance_id: str = new_mocked_ec2_with_tags({ "Key": "PatchGroup", "Value": "PatchGroup_1" })
@@ -30,7 +30,7 @@ def test_update_baseline_lambda_happy_path():
     assert new_boto3_ec2_resource().Instance(instance_id).tags == [{"Key":"PatchGroup", "Value":"CRITICAL_PatchGroup_1"}] # tags modified (add CRITICAL prefix)
 
 @mock_ec2
-def test_update_baseline_lambda_no_matching_ec2s():
+def test_update_baseline_lambda_no_matching_ec2s(mock_aws_environment):
     # setup
     from critical_patching import update_baseline_function # accept the moto mocked client, need __init__.py files for module reference
     instance_id: str = new_mocked_ec2_with_tags({ "Key": "PatchGroup", "Value": "PatchGroup_22" })
